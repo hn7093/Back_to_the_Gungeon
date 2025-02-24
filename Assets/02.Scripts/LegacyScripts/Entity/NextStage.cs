6be396cd5c82cf373236b52b1005184b543d5ade
Pgt103 <ddh06095@gmail.com>
2025-02-24 13:59:58 +0900

    [feat] 스테이지 변경 로직 추가중

---
+++ Assets/02.Scripts/LegacyScripts/Entity/NextStage.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class NextStage : MonoBehaviour
+{
+    public GameObject closeDoor;
+    public GameObject openDoor;
+    
+    StageManager stageManager;
+    
+    public void DoorOpen()
+    {
+        closeDoor.gameObject.SetActive(false);
+        openDoor.gameObject.SetActive(true);
+    }
+
+    public void DoorClose()
+    {
+        closeDoor.gameObject.SetActive(true);
+        openDoor.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        openDoor = collision.gameObject;
+        
+        // 페이드 아웃 후
+        Destroy(stageManager.currentStage); // 현재 맵 파괴
+        // 페이드 인 후
+        stageManager.GenerateNewStage();
+        // 플레이어 좌표값도 변경해야함
+    }
+
+    
+}
---
+++ Assets/02.Scripts/LegacyScripts/Manager/StageManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    public List<GameObject> stage = new List<GameObject>();
+    public GameObject currentStage;
+    
+    NextStage nextStage;
+
+    private int stageCount = 0;
+    private int randomMapIndex;
+    private int lastMapIndex = -1; // 첫맵부터 랜덤맵인덱스랑 겹쳐서 같은 맵 판정을 받으면 안되기 때문에 마이너스로 초기화   
+    void Start()
+    {
+        GenerateNewStage();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StageClear();
+    }
+
+    void StageClear()
+    {
+        if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0) // 현재 씬에 적이 없을 떄
+        {
+            Debug.Log("StageClear!");
+            nextStage.DoorOpen();
+        }
+        else
+        {
+            nextStage.DoorClose();
+        }
+    }
+    
+    public void GenerateNewStage()
+    {
+        stageCount++;
+        
+        if (currentStage != null)
+        {
+            Destroy(currentStage);
+        }
+
+        do // 연속으로 같은 맵이 나오지 않도록 방지한다
+        {
+            randomMapIndex = Random.Range(0, stage.Count);
+        } while (randomMapIndex == lastMapIndex);
+        
+        lastMapIndex = randomMapIndex;
+
+        if (stageCount % 3 == 0)
+        {
+            currentStage = Instantiate(stage[randomMapIndex]); // 보상 맵 출현
+        }
+        else if (stageCount % 10 == 0)
+        {
+            currentStage = Instantiate(stage[randomMapIndex]); // 보스 스테이지 지정
+        }
+        else
+        {
+         currentStage = Instantiate(stage[randomMapIndex]); // 일반맵 생성   
+        }
+    }
+    
+}
