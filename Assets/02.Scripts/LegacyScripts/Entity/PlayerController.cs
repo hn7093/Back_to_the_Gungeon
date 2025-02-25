021fafc195e3b81b8d4b78af261e742ea4bcaaa6
judelover27 <judelover27@gmail.com>
2025-02-25 09:39:04 +0900

    [feat] 마우스 이동조작

--- Assets/02.Scripts/LegacyScripts/Entity/PlayerController.cs
+++ Assets/02.Scripts/LegacyScripts/Entity/PlayerController.cs
@@ -3,17 +3,52 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : BaseController
 {
 
     private List<BaseController> enemyList; // 적 리스트
+
+    private Vector2 startTouchPosition;
+    private Vector2 currentTouchPosition;
+    private bool isDragging = false;
+    private float dragThreshold = 1f;
+
     protected override void HandleAction()
+    { HandleKeyboardInput(); }
+
+    protected void HandleKeyboardInput()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         movementDirection = new Vector2(horizontal, vertical).normalized;
 
+    }
+
+    private void HandleMouseInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startTouchPosition = Input.mousePosition;
+            isDragging = true;
+        }
+
+        if (isDragging && Input.GetMouseButton(0))
+        {
+            currentTouchPosition = Input.mousePosition;
+            float dragDistance = Vector2.Distance(startTouchPosition, currentTouchPosition);
+            if (dragDistance > dragThreshold)
+                movementDirection = (currentTouchPosition - startTouchPosition).normalized;
+            else
+                movementDirection = Vector2.zero;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+            movementDirection = Vector2.zero;
+        }
     }
 
     protected override void SetIsAttacking()
