ad58e55efcfed62f3aff8c1c7c43fc9a40e6417c
Pgt103 <ddh06095@gmail.com>
2025-02-25 19:09:36 +0900

    [feat] 나무통에 접촉시 폭발하며 주위에 넉백 효과 추가
    하지만 현재는 멈춰있는 오브젝트만 적용되고 움직이는(ex:플레이어)는 제대로 적용되지 않는 상태이다

--- Assets/02.Scripts/LegacyScripts/Entity/Trap.cs
+++ Assets/02.Scripts/LegacyScripts/Entity/Trap.cs
@@ -8,18 +8,22 @@ public class Trap : MonoBehaviour
 {
     public GameObject trap;
     public GameObject trapEffect;
+    public int trapDamage = 20;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             Debug.Log("Trap activated");
+            // 폭발 이펙트 생성
             GameObject effect = Instantiate(trapEffect, transform.position, Quaternion.identity);
-            ParticleSystem particleSystem = trapEffect.GetComponent<ParticleSystem>();
-
+            ParticleSystem particleSystem = effect.GetComponent<ParticleSystem>();
+            
+            // 닿은 플레이어에게 데미지
+            
             if (particleSystem != null)
             {
-                Destroy(effect, particleSystem.main.duration + 0.5f);
+                Destroy(effect, particleSystem.main.duration + 0.5f); // 이펙트 재생 후 0.5초 뒤 생성된 이펙트 제거
             }
             Destroy(trap); // 사용한 트랩 제거
         }
