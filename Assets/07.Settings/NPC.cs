43552b93f965c399a1f8459d64689dfc08bfd438
Junebeom Lee <junebeom.dev@gmail.com>
2025-02-26 20:42:41 +0900

    test: NPC 추가

--- Assets/07.Settings/NPC.cs
+++ Assets/07.Settings/NPC.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    private void Start()
+
+    public GameObject Player;
+
+    // 플레이어 시작 부분에 추가 필요
+    private void Awake()
     {
+        SystemManager.Instance.RegisterPlayer(Player);
     }
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SystemManager.Instance.UIManager.OpenPage(PageType.IVENTORY_PAGE);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SystemManager.Instance.UIManager.OpenPage(PageType.PAUSE_PAGE);
+        }
+    }
+    
+    // NPC 또는 문을 통과할 때
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        SystemManager.Instance.RegisterPlayer(collision.gameObject);
-
-        // SystemManager.Instance.EventManager.OpenEventPage(PageType.ANGEL_PAGE).ContinueWith(result =>
-        // {
-            // Debug.Log(result.Result);
-        // });
+        SystemManager.Instance.EventManager.OpenEventPage(PageType.ANGEL_PAGE);
+        // SystemManager.Instance.EventManager.OpenEventPage(PageType.DEVIL_PAGE);
+        // SystemManager.Instance.EventManager.OpenEventPage(PageType.ROULETTE_PAGE);
     }
 }
